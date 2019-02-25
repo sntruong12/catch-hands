da42e4ab238825ee6372413d2a543aac4450347c
Son Truong <sntruong12@gmail.com>
2019-02-24 18:01:29 -0600

    rename file from users to index

---
+++ controllers/index.js
@@ -0,0 +1,32 @@
+var rp = require('request-promise');
+
+var User = require('../models/user');
+
+module.exports = {
+  index
+}
+
+function index(req, res, next) {
+  var options = {
+    // figure out how to get dynamic url
+    uri: 'http://localhost:3000/api/characters',
+    json: true
+  }
+
+  rp(options)
+    .then(characters => {
+      res.render('index', { 
+        title: 'Catch Hands',
+        user: req.user,
+        characters
+      });
+    })
+    .catch(err => {
+      console.log(err, 'error getting characters');
+      res.render('index', {
+        title: 'Catch Hands',
+        user: req.user
+      })
+    })
+
+}
