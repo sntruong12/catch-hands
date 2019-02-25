e46c7c1f16420c53fdaf03496d5a3ebd2d1deee0
Son Truong <sntruong12@gmail.com>
2019-02-25 16:18:45 -0600

    refactor the mains property with the complete list of fighters

--- models/user.js
+++ models/user.js
@@ -9,9 +9,9 @@ var userSchema = new Schema({
   email: {
     type: String
   },
-  fighters: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Fighter'
+  mains: [{
+    type: String,
+    enum: ["Bayonetta", "Bowser", "Bowser Jr.", "Captain Falcon", "Chrom", "Cloud", "Corrin", "Daisy", "Dark Pit", "Dark Samus", "Diddy Kong", "Donkey Kong", "Dr. Mario", "Duck Hunt", "Falco", "Fox", "Ganondorf", "Greninja", "Ice Climbers", "Ike", "Inkling", "Incineroar", "Isabelle", "Jigglypuff", "Joker", "Ken", "King Dedede", "King K.Rule", "Kirby", "Link", "Little Mac", "Lucario", "Lucas", "Lucina", "Luigi", "Mario", "Marth", "Mega Man", "Meta Knight", "Mewtwo", "Mii Brawler", "Mii Gunner", "Mii Fighter", "Mr. Game & Watch", "Ness", "Captain Olimar", "Pac-Man", "Palutena", "Peach", "Pichu", "Pikachu", "Piranha Plant", "Pit", "Pokémon Trainer", "Richter", "Ridley", "R.O.B.", "Robin", "Rosalina", "Roy", "Ryu", "Samus", "Sheik", "Shulk", "Simon", "Snake", "Sonic", "Toon Link", "Villager", "Wario", "Wii Fit Trainer", "Wolf", "Yoshi", "Young Link", "Zelda", "Zero Suit Samus"]
   }],
   googleId: String
 }, {
