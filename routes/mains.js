4f6b9650c8fbd2c3bbb217c199a66418b5a6e251
Son Truong <sntruong12@gmail.com>
2019-02-26 16:18:53 -0600

    add routes for adding a main and deleting a main

--- routes/mains.js
+++ routes/mains.js
@@ -5,6 +5,8 @@ var mainsCtrl = require('../controllers/mains');
 router.get('/', mainsCtrl.index);
 
 // add a fighter to the user's mains
-router.get('/add/:id', mainsCtrl.addFighter);
+router.get('/add/:id', mainsCtrl.addMain);
+// delete a fighter from the user's mains
+router.delete('/:id', mainsCtrl.destroyMain)
 
 module.exports = router;
