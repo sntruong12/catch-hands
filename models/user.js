15d8af0e5329126d0fe5a1a1c921e2a06ad0d26b
Son Truong <sntruong12@gmail.com>
2019-02-26 16:15:12 -0600

    redo model to help with add and delete functions for user's mains

--- models/user.js
+++ models/user.js
@@ -10,8 +10,19 @@ var userSchema = new Schema({
     type: String
   },
   mains: [{
-    type: String,
-    enum: ["Bayonetta", "Bowser", "Bowser Jr.", "Captain Falcon", "Chrom", "Cloud", "Corrin", "Daisy", "Dark Pit", "Dark Samus", "Diddy Kong", "Donkey Kong", "Dr. Mario", "Duck Hunt", "Falco", "Fox", "Ganondorf", "Greninja", "Ice Climbers", "Ike", "Inkling", "Incineroar", "Isabelle", "Jigglypuff", "Joker", "Ken", "King Dedede", "King K.Rule", "Kirby", "Link", "Little Mac", "Lucario", "Lucas", "Lucina", "Luigi", "Mario", "Marth", "Mega Man", "Meta Knight", "Mewtwo", "Mii Brawler", "Mii Gunner", "Mii Fighter", "Mr. Game & Watch", "Ness", "Captain Olimar", "Pac-Man", "Palutena", "Peach", "Pichu", "Pikachu", "Piranha Plant", "Pit", "Pokémon Trainer", "Richter", "Ridley", "R.O.B.", "Robin", "Rosalina", "Roy", "Ryu", "Samus", "Sheik", "Shulk", "Simon", "Snake", "Sonic", "Toon Link", "Villager", "Wario", "Wii Fit Trainer", "Wolf", "Yoshi", "Young Link", "Zelda", "Zero Suit Samus"]
+    id: {
+      type: Number,
+    },
+    name: {
+      type: String,
+      enum: ["Bayonetta", "Bowser", "Bowser Jr.", "Captain Falcon", "Chrom", "Cloud", "Corrin", "Daisy", "Dark Pit", "Dark Samus", "Diddy Kong", "Donkey Kong", "Dr. Mario", "Duck Hunt", "Falco", "Fox", "Ganondorf", "Greninja", "Ice Climbers", "Ike", "Inkling", "Incineroar", "Isabelle", "Jigglypuff", "Joker", "Ken", "King Dedede", "King K.Rule", "Kirby", "Link", "Little Mac", "Lucario", "Lucas", "Lucina", "Luigi", "Mario", "Marth", "Mega Man", "Meta Knight", "Mewtwo", "Mii Brawler", "Mii Gunner", "Mii Fighter", "Mr. Game & Watch", "Ness", "Captain Olimar", "Pac-Man", "Palutena", "Peach", "Pichu", "Pikachu", "Piranha Plant", "Pit", "Pokémon Trainer", "Richter", "Ridley", "R.O.B.", "Robin", "Rosalina", "Roy", "Ryu", "Samus", "Sheik", "Shulk", "Simon", "Snake", "Sonic", "Toon Link", "Villager", "Wario", "Wii Fit Trainer", "Wolf", "Yoshi", "Young Link", "Zelda", "Zero Suit Samus"]
+    },
+    image_url: {
+      type: String,
+    },
+    combos: [{
+      type: String,
+    }]
   }],
   googleId: String
 }, {
