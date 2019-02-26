1cb96e76dab14eb2d3dcc6492a1fe59ad556657e
Son Truong <sntruong12@gmail.com>
2019-02-26 16:18:25 -0600

    refactor add mains function and create the destroymain and destroyallmains function

--- controllers/mains.js
+++ controllers/mains.js
@@ -4,7 +4,9 @@ var User = require('../models/user');
 
 module.exports = {
   index,
-  addFighter
+  addMain,
+  destroyMain,
+  destroyAllMains
 }
 
 function index(req, res) {
@@ -19,7 +21,7 @@ function index(req, res) {
     })
 }
 
-function addFighter(req, res) {
+function addMain(req, res) {
   var options = {
     // figure out how to get dynamic url
     uri: `http://localhost:3000/api/fighters/${req.params.id}`,
@@ -27,31 +29,65 @@ function addFighter(req, res) {
   }
 
   rp(options)
-    .then(character => {
-      return character.name
+    .then(fighter => {
+      console.log(fighter, "API RESPONSE SON!")
+      var newMain = {
+        "id": fighter.id,
+        "name": fighter.name,
+        "image_url": fighter.image_url,
+        "combos": fighter.combos
+      }
+      return newMain
     })
     .catch(err => {
       console.log('error fetching fighter: ', err)
     })
-    .then(name => {
-      User.findOne( {googleId: req.user.googleId} )
-        .then(user => {
-          console.log(user.mains)
-
-          // check if fighter is already in mains array
-          if(user.mains.includes(name)) {
-            console.log('same fighter cannot be added twice')
-            res.redirect('/mains')
-          } else {
-            console.log('fighter has been added to user\'s mains')
-            user.mains.push(name);
-            user.save();
-            res.redirect('/mains')
-          }
-
-        })
+    .then(newM => {
+      return User.findOneAndUpdate(
+        { googleId: req.user.googleId },
+        { $push: { mains: newM } },
+        { new: true } 
+      )
     })
     .catch(err => {
-      console.log('error finding user: ', err)
+      console.log('error updating user\'s mains', err)
+    })
+    .then(() => {
+      res.redirect('/mains')
+    })
+}
+
+function destroyMain(req, res) {
+  User.findOne( { googleId: req.user.googleId } )
+    .then(user => {
+      var mains = user.mains
+      var filteredMains = mains.filter(main => {
+        return main.id !== parseInt(req.params.id)
+      })
+      return filteredMains
+    })
+    .then(filteredM => {
+      return User.findOneAndUpdate(
+        { googleId: req.user.googleId },
+        { $set: { mains: filteredM } },
+        { new: true } 
+      )
+    })
+    .then(() => {
+      res.redirect('/mains')
+    })
+}
+
+function destroyAllMains(req, res) {
+  User.findOneAndUpdate(
+      { googleId: req.user.googleId },
+      { $set: 
+        { mains: [] },
+      },
+      { new: true }
+    )
+    .then(removedMains => {
+      console.log(removedMains);
+      res.redirect('/mains')
     })
 }
