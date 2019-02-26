c3c65b791ed5de182cafa9f8e0761dbb819e8add
Son Truong <sntruong12@gmail.com>
2019-02-25 23:58:16 -0600

    add and configure method-override npm module

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var passport = require('passport');
 var logger = require('morgan');
+var methodOverride = require('method-override');
 var cors = require('cors');
 
 // load the .env variables
@@ -43,6 +44,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
 
 // user routes
 app.use('/', indexRouter);
