9ba5006218f1bd91a151b7878187d5ef6d7542ba
Son Truong <sntruong12@gmail.com>
2019-02-26 16:17:27 -0600

    remove console log profile

--- config/passport.js
+++ config/passport.js
@@ -9,7 +9,6 @@ passport.use(new GoogleStrategy (
   callbackURL: process.env.GOOGLE_CALLBACK
   }, 
   function(accessToken, refreshToken, profile, cb) {
-    console.log(profile)
     User.findOne({ 'googleId': profile.id }, function(err, user) {
       if (err) return cb(err);
       if (user) {
