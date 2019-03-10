4ee37585d06e01419e63e886f9127334e085de4a
Son Truong <sntruong12@gmail.com>
2019-03-09 23:44:16 -0600

    add semicolons

--- routes/api/fighters.js
+++ routes/api/fighters.js
@@ -5,8 +5,8 @@ var fightersCtrl = require('../../controllers/api/fighters');
 router.get('/fighters', fightersCtrl.getAllFighters);
 router.get('/fighters/:id', fightersCtrl.getOneFighter);
 // router.post('/fighters', fightersCtrl.createFighter);
-// router.put('/fighters/:id', fightersCtrl.updateFighter)
-// router.delete('/fighters/:id', fightersCtrl.deleteFighter)
+// router.put('/fighters/:id', fightersCtrl.updateFighter);
+// router.delete('/fighters/:id', fightersCtrl.deleteFighter);
 
 // api reference route
 router.get('/reference', fightersCtrl.index)
